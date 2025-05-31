787ab29ca9f4845fff7666b5d2f27d42a9cb6871
Utkarshnegi2k5 <utkarshnegi05@gmail.com>
2025-06-01 01:16:38 +0530

    Created a new component and changed the title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { ButtonComponent } from './MyComponent/button/button.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet,ButtonComponent],
 })
 export class AppComponent {
   constructor() {}
